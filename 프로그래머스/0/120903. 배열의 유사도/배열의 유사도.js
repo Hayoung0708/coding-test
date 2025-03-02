a0c066bd9aeac88df24eecb814efa24b203c384d
Hayoung Kang <85282786+Hayoung0708@users.noreply.github.com>
2025-03-02 19:13:52 +0900

    [level 0] Title: 배열의 유사도, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -0,0 +1,12 @@
+function solution(s1, s2) {
+    let answer = 0;
+    for (i of s1) {
+        for (j of s2) {
+            if (i == j) {
+                answer++;
+                break;
+            }
+        }
+    }
+    return answer;
+}
