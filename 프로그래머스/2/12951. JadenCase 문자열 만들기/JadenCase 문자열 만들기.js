847ebf690375ab880e75747895b7bbab92559650
Hayoung Kang <85282786+Hayoung0708@users.noreply.github.com>
2025-03-06 11:06:47 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,7 +1,7 @@
 function solution(s) {
     let answer = "";
     for (i in s) {
-        if (i == 0 || i != 0 && s[i-1] == ' ') {
+        if (i == 0 || s[i-1] == ' ') {
             answer += s[i].toUpperCase();
         }
         else {
