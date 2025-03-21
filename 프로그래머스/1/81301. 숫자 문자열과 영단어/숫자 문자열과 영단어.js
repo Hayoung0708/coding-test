f36f11749067daa297d2c2bbe893862e97bd73c4
Hayoung Kang <85282786+Hayoung0708@users.noreply.github.com>
2025-03-21 16:30:12 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,21 +1,9 @@
 function solution(s) {
     const num = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-    answer = "";
     
-    for (i in s) {
-        if (!isNaN(s[i])) {
-            answer += s[i];
-        }
-        else {
-            for (j in num) {
-                if (s.substring(i).startsWith(num[j])) {
-                    answer += j;
-                    i += num[j].length;
-                    break;
-                }
-            }
-        }
+    for(i in num) {
+        s = s.replaceAll(num[i], i);
     }
     
-    return +answer;
+    return +s;
 }
