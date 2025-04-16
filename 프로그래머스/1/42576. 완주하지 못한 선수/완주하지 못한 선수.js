0ad593a1ba72beb6da34f7a40c8c323a6318272c
Hayoung Kang <85282786+Hayoung0708@users.noreply.github.com>
2025-04-16 16:28:19 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 101.65 ms, Memory: 56.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -2,7 +2,7 @@ function solution(participant, completion) {
     participant.sort();
     completion.sort();
     
-    for (let i = 0; i < participant.length - 1; i++) {
+    for (let i = 0; i < completion.length; i++) {
         if (participant[i] !== completion[i]) {
             return participant[i];
         }
