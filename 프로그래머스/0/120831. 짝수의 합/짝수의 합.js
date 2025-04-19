0de3a541ad5019f7a13caf5e28db679c31c2d092
Hayoung Kang <85282786+Hayoung0708@users.noreply.github.com>
2025-04-19 23:25:55 +0900

    [level 0] Title: 짝수의 합, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
@@ -1,6 +1,6 @@
 function solution(n) {
-    var sum = 0;
-    for (var i = 0; i <= n; i++) {
+    let sum = 0;
+    for (let i = 0; i <= n; i++) {
         if (i%2 == 0) {
             sum += i;
         }
