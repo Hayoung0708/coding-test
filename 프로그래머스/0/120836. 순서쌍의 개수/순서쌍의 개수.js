d3c681a055c556528821f81cb2e1117cbc332a5d
Hayoung Kang <85282786+Hayoung0708@users.noreply.github.com>
2025-04-21 09:01:58 +0900

    [level 0] Title: 순서쌍의 개수, Time: 33.81 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,6 +1,6 @@
 function solution(n) {
-    var answer = 1;
-    for (i = 1; i < n; i++) {
+    let answer = 1;
+    for (let i = 1; i < n; i++) {
         if (n % i === 0) {
             answer++;
         }
