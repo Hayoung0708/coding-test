5f92ad2db98d3f08ed7043f3bcf205bbedc119a7
Hayoung Kang <85282786+Hayoung0708@users.noreply.github.com>
2025-04-23 15:39:02 +0900

    [level 1] Title: K번째수, Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,8 +1,12 @@
 function solution(array, commands) {
     let answer = []
     for (let c of commands) {
-        let subArr = array.slice(c[0]-1, c[1]).sort((a, b) => a - b);
-        answer.push(subArr[c[2]-1]);
+        answer.push(
+            array
+            .slice(c[0]-1, c[1])
+            .sort((a, b) => a - b)
+            [c[2]-1]
+        );
     }
     
     return answer;
