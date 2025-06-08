152340f0f2b8d8f0a0fea8d410203861e4cafd2f
Hayoung Kang <85282786+Hayoung0708@users.noreply.github.com>
2025-06-08 23:38:48 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
@@ -1,3 +1,1 @@
-function solution(numbers) {
-    return numbers.reduce((sum, i) => sum + i, 0) / numbers.length;
-}
+const solution = (numbers) => numbers.reduce((sum, i) => sum + i, 0) / numbers.length
