e4b27600d47abc13c11a583152bb925496a076a1
Hayoung Kang <85282786+Hayoung0708@users.noreply.github.com>
2025-07-03 23:06:41 +0900

    [level 0] Title: 모음 제거, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.js
@@ -1,5 +1,5 @@
-function solution(my_string) {
-    var answer = '';
+const solution = (my_string) => {
+    let answer = '';
     for (i of my_string) {
         if (i != "a" && i != "e" && i != "i" && i != "o" && i != "u") {
             answer += i;
