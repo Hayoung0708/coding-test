05bb6a36b3b0924fdb6a250d885a84c7cac7f74f
Hayoung Kang <85282786+Hayoung0708@users.noreply.github.com>
2025-08-02 22:40:51 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.js
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.js
@@ -1,5 +1,5 @@
-function solution(n, numlist) {
-    var answer = [];
+const solution = (n, numlist) => {
+    let answer = [];
     for (i of numlist) {
         if (i % n == 0) {
             answer.push(i);
