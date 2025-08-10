418f2b24baf68f5f0a5dc51f0e8674de15f5e1ec
Hayoung Kang <85282786+Hayoung0708@users.noreply.github.com>
2025-08-10 23:23:17 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,3 +1,1 @@
-function solution(my_string) {
-    return my_string.split('').sort().filter(char => !isNaN(char)).map(Number);
-}
+const solution = (my_string)  =>  my_string.split('').sort().filter(char => !isNaN(char)).map(Number)
