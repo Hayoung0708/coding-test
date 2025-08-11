bd55cc203b77efe8ed8f8b15a8c20ed53c8ba89c
Hayoung Kang <85282786+Hayoung0708@users.noreply.github.com>
2025-08-11 23:02:09 +0900

    [level 0] Title: 주사위의 개수, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.js
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.js
@@ -1,3 +1,2 @@
-function solution(box, n) {
-    return Math.floor(box[0] / n) * Math.floor(box[1] / n) * Math.floor(box[2] / n);
-}
+const solution = (box, n) => 
+     Math.floor(box[0] / n) * Math.floor(box[1] / n) * Math.floor(box[2] / n);
