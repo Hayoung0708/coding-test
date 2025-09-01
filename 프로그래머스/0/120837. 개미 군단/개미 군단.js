190b8cc8ae382c901c7139c361a9dfb20dc2e95a
Hayoung Kang <85282786+Hayoung0708@users.noreply.github.com>
2025-09-01 23:21:33 +0900

    [level 0] Title: 개미 군단, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -0,0 +1,10 @@
+const solution = (hp) => {
+    let answer = 0;
+    
+    answer += Math.floor(hp / 5);
+    hp = hp % 5;
+    answer += Math.floor(hp / 3);
+    hp = hp % 3;
+    
+    return answer + hp;
+}
