af8bf0a60fe1f28b6945640f74f3d7fd6cf521d0
Hayoung Kang <85282786+Hayoung0708@users.noreply.github.com>
2025-09-04 23:50:05 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,4 +1,4 @@
-function solution(numbers) {
+const solution = (numbers) => {
     numbers.sort((a, b) => a - b);
 
     return Math.max(numbers[0] * numbers[1], numbers[numbers.length - 1] * numbers[numbers.length - 2]);
