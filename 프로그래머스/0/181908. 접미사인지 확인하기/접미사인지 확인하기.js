e24fe31edb62d667ca85a4deba521810ab8a52ea
Hayoung Kang <85282786+Hayoung0708@users.noreply.github.com>
2025-09-11 16:11:06 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,2 +1,1 @@
-const solution = (my_string, is_suffix) => 
-    my_string.slice(my_string.length - is_suffix.length, my_string.length + 1) === is_suffix ? 1 : 0
+const solution = (my_string, is_suffix) => my_string.endsWith(is_suffix) ? 1 : 0
