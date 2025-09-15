e3ff50d7da9297162122f87975db7d8348396d3d
Hayoung Kang <85282786+Hayoung0708@users.noreply.github.com>
2025-09-15 23:10:43 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.29 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.js
@@ -0,0 +1,9 @@
+const solution = (my_strings, parts) => {
+    let answer = '';
+    
+    for (i in my_strings) {
+        answer += my_strings[i].slice(parts[i][0], parts[i][1] + 1);
+    }
+    
+    return answer;
+}
