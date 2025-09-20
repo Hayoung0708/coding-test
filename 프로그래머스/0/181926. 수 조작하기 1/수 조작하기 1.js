1361097fd182cfb34bbee7d061d17ca38ea43fd9
Hayoung Kang <85282786+Hayoung0708@users.noreply.github.com>
2025-09-20 22:42:37 +0900

    [level 0] Title: 수 조작하기 1, Time: 25.35 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,26 @@
+const solution = (n, control) => {
+    let answer = n;
+    
+    control.split('').map((i) => {
+        switch (i) {
+            case 'w': {
+                answer += 1;
+                break;
+            }
+            case 's': {
+                answer -= 1;
+                break;
+            }
+            case 'd': {
+                answer += 10;
+                break;
+            }
+            case 'a': {
+                answer -= 10;
+                break;
+            }
+        }
+    })
+                          
+    return answer;
+}
