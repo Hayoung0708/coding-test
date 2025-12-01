1dbecbf4126e5496a0ff46420817bb9100360607
Hayoung Kang <85282786+Hayoung0708@users.noreply.github.com>
2025-12-01 23:20:28 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 28.58 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -1,4 +1,4 @@
-const solution = (arr) => arr.map((i) => {
+const solution = (arr) => arr.map(i => {
     if (i % 2 === 0 && i >= 50) return i / 2;
     if (i % 2 === 1 && i < 50) return i * 2
     return i
