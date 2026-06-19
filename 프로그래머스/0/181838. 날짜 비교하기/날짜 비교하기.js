cce0498c9378b3abe33b3f2bfef658dc7e4deb10
Hayoung Kang <85282786+Hayoung0708@users.noreply.github.com>
2026-06-19 13:12:55 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.08 ms, Memory: 43.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -0,0 +1,10 @@
+const solution = (date1, date2) => {
+    for (i in date1) {
+        if (date1[i] < date2[i])
+            return 1;
+        if (date1[i] > date2[i])
+            return 0;
+    }
+    
+    return 0;
+}
